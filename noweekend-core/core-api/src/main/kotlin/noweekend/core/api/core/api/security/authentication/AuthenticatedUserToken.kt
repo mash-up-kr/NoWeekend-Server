d22e6e8e2bc3267452cd5eb324b489d07e305817
jay.park <once29@naver.com>
2025-05-18 11:07:03 +0900

    chore: reformat codes

--- noweekend-core/core-api/src/main/kotlin/noweekend/core/api/core/api/security/authentication/AuthenticatedUserToken.kt
+++ noweekend-core/core-api/src/main/kotlin/noweekend/core/api/core/api/security/authentication/AuthenticatedUserToken.kt
@@ -2,7 +2,7 @@ package noweekend.core.api.core.api.security.authentication
 
 import org.springframework.security.authentication.AbstractAuthenticationToken
 
-class AuthenticatedUserToken(private val token: String, val principal: String): AbstractAuthenticationToken(null) {
+class AuthenticatedUserToken(private val token: String, val principal: String) : AbstractAuthenticationToken(null) {
     override fun getCredentials(): Any = token
     override fun getPrincipal(): Any = principal
     override fun isAuthenticated(): Boolean = true
