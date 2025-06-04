d156ee04008a0f9953073d107953c9a5fe52a174
toychip <dlawnsgud427@naver.com>
2025-06-04 15:51:28 +0900

    refactor: simple code by Supplier

--- noweekend-mcp/mcp-host/src/main/kotlin/noweekend/mcphost/config/MyAgentState.kt
+++ noweekend-mcp/mcp-host/src/main/kotlin/noweekend/mcphost/config/MyAgentState.kt
@@ -3,7 +3,6 @@ package noweekend.mcphost.config
 import org.bsc.langgraph4j.state.AgentState
 import org.bsc.langgraph4j.state.Channel
 import org.bsc.langgraph4j.state.Channels
-import java.util.function.Supplier
 
 class MyAgentState(initData: Map<String, Any>) : AgentState(initData) {
 
@@ -19,9 +18,9 @@ class MyAgentState(initData: Map<String, Any>) : AgentState(initData) {
     companion object {
         @JvmStatic
         val SCHEMA: Map<String, Channel<*>> = mapOf(
-            "question" to Channels.base(Supplier { "" }),
-            "chat_response" to Channels.base(Supplier { "" }),
-            "weather_json" to Channels.base(Supplier { "" }),
+            "question" to Channels.base { "" },
+            "chat_response" to Channels.base { "" },
+            "weather_json" to Channels.base { "" },
         )
     }
 }
