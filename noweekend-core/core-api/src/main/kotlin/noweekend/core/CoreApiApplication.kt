e52b3df875cb49d33dfc0475a4a9fe4327002b61
toychip <dlawnsgud427@naver.com>
2025-06-06 00:04:21 +0900

    feat: add ComponentScan range

--- noweekend-core/core-api/src/main/kotlin/noweekend/core/CoreApiApplication.kt
+++ noweekend-core/core-api/src/main/kotlin/noweekend/core/CoreApiApplication.kt
@@ -3,9 +3,17 @@ package noweekend.core
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
 
 @ConfigurationPropertiesScan
 @SpringBootApplication
+@ComponentScan(
+    basePackages = [
+        "noweekend.core.api",
+        "noweekend.core.domain",
+        "noweekend.client",
+    ],
+)
 class CoreApiApplication
 
 fun main(args: Array<String>) {
