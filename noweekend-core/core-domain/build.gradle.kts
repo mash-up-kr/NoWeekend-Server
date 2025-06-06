7a689254903bd8eba09da1e37f9fb38e94bd3d79
toychip <dlawnsgud427@naver.com>
2025-06-06 20:12:57 +0900

    test: test2

--- noweekend-core/core-domain/build.gradle.kts
+++ noweekend-core/core-domain/build.gradle.kts
@@ -7,9 +7,7 @@ dependencies {
     implementation("org.springframework:spring-tx")
 
     // JWT
-    implementation("io.jsonwebtoken:jjwt-api:0.12.6")
-    implementation("io.jsonwebtoken:jjwt-impl:0.12.6")
-    implementation("io.jsonwebtoken:jjwt-jackson:0.12.6")
+    implementation("io.jsonwebtoken:jjwt-api:0.11.5")
 
     // jasypt
     implementation("com.github.ulisesbocchio:jasypt-spring-boot-starter:3.0.5")
