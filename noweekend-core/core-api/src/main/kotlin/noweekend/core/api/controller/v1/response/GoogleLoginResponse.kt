b5c9c02ff4e1bbd56cb01d2e0ecc103ee0de6d08
toychip <dlawnsgud427@naver.com>
2025-06-07 10:44:30 +0900

    feat: update Swagger docs for Google login with more details after review

--- noweekend-core/core-api/src/main/kotlin/noweekend/core/api/controller/v1/response/GoogleLoginResponse.kt
+++ noweekend-core/core-api/src/main/kotlin/noweekend/core/api/controller/v1/response/GoogleLoginResponse.kt
@@ -4,10 +4,10 @@ import io.swagger.v3.oas.annotations.media.Schema
 
 @Schema(description = "구글 로그인 응답 DTO")
 data class GoogleLoginResponse(
-    @Schema(description = "구글 이메일", example = "test@example.com")
+    @field:Schema(description = "구글 이메일", example = "test@example.com")
     val email: String,
-    @Schema(description = "이미 가입된 회원 여부")
+    @field:Schema(description = "이미 가입된 회원 여부")
     val exists: Boolean,
-    @Schema(description = "발급된 액세스 토큰", example = "eyJhbGciOiJIUzI1NiIsInR5cCI6...")
+    @field:Schema(description = "발급된 액세스 토큰", example = "eyJhbGciOiJIUzI1NiIsInR5cCI6...")
     val accessToken: String,
 )
