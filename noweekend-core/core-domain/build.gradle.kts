2e4d420a3c92fca0422486e02c1b6a9f890b66bb
toychip <dlawnsgud427@naver.com>
2025-06-09 23:44:44 +0900

    fix: google oauth login (#15)

--- noweekend-core/core-domain/build.gradle.kts
+++ noweekend-core/core-domain/build.gradle.kts
@@ -8,6 +8,8 @@ dependencies {
 
     // JWT
     implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
 
     // jasypt
     implementation("com.github.ulisesbocchio:jasypt-spring-boot-starter:3.0.5")
