d04a47b9ba62c851f376a8b08e04c80723f54009
jay.park <once29@naver.com>
2025-06-18 00:48:02 +0900

    fix: jwt library dependency scope

--- noweekend-core/core-domain/build.gradle.kts
+++ noweekend-core/core-domain/build.gradle.kts
@@ -7,7 +7,7 @@ dependencies {
     implementation("org.springframework:spring-tx")
 
     // JWT
-    implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+    api("io.jsonwebtoken:jjwt-api:0.11.5")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
 
