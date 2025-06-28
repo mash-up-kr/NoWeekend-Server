b96207d6bb9d05996a688ff107508172d49e0ac4
toychip <dlawnsgud427@naver.com>
2025-06-28 20:29:50 +0900

    docs: add onboarding profile(nickname, birth)

--- noweekend-core/core-api/src/main/kotlin/noweekend/core/api/controller/v1/UserController.kt
+++ noweekend-core/core-api/src/main/kotlin/noweekend/core/api/controller/v1/UserController.kt
@@ -1,6 +1,7 @@
 package noweekend.core.api.controller.v1
 
 import io.swagger.v3.oas.annotations.tags.Tag
+import noweekend.core.api.controller.v1.request.OnboardingRequest
 import noweekend.core.api.controller.v1.request.ScheduleRequest
 import noweekend.core.support.response.ApiResponse
 import org.springframework.validation.annotation.Validated
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@Tag(name = "유저 컨트롤러", description = "유저 관련 API")
+@Tag(name = "온보딩", description = "온보딩 등록 API")
 @RestController
 @RequestMapping("/api/v1/user/onboarding")
 class UserController() : UserControllerDocs {
@@ -20,6 +21,15 @@ class UserController() : UserControllerDocs {
         val enumList = request.validatedScheduleTags()
         return ApiResponse.success(
             "일정 등록이 성공적으로 완료되었습니다.",
+        )
+    }
+
+    @PostMapping("/profile")
+    override fun submitProfile(
+        @RequestBody request: OnboardingRequest
+    ): ApiResponse<String> {
+        return ApiResponse.success(
+            "닉네임 및 생년월일 등록이 성공적으로 완료되었습니다.",
         )
     }
 }
--- noweekend-core/core-api/src/main/kotlin/noweekend/core/api/controller/v1/UserControllerDocs.kt
+++ noweekend-core/core-api/src/main/kotlin/noweekend/core/api/controller/v1/UserControllerDocs.kt
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.media.Content
 import io.swagger.v3.oas.annotations.media.ExampleObject
 import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.parameters.RequestBody
+import noweekend.core.api.controller.v1.request.OnboardingRequest
 import noweekend.core.api.controller.v1.request.ScheduleRequest
 import noweekend.core.support.response.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponse as SwaggerApiResponse
@@ -58,4 +59,46 @@ interface UserControllerDocs {
         ],
     )
     fun saveUserSchedules(request: ScheduleRequest): ApiResponse<String>
+
+    @Operation(
+        summary = "온보딩: 닉네임/생년월일 등록",
+        description = "유저가 온보딩 시 닉네임과 생년월일을 등록합니다.",
+        requestBody = RequestBody(
+            required = true,
+            content = [Content(
+                mediaType = "application/json",
+                schema = Schema(implementation = OnboardingRequest::class),
+                examples = [ExampleObject(
+                    name = "예시 요청",
+                    value = """
+                        {
+                          "nickname": "김매송..",
+                          "birthDate": "19991213"
+                        }
+                    """
+                )]
+            )]
+        ),
+        responses = [
+            SwaggerApiResponse(
+                responseCode = "200",
+                description = "닉네임 및 생년월일 등록 성공",
+                content = [Content(
+                    mediaType = "application/json",
+                    schema = Schema(implementation = String::class),
+                    examples = [ExampleObject(
+                        name = "예시 응답",
+                        value = """
+                            "닉네임 및 생년월일 등록이 성공적으로 완료되었습니다."
+                        """,
+                    )]
+                )]
+            ),
+            SwaggerApiResponse(
+                responseCode = "400",
+                description = "잘못된 요청"
+            ),
+        ]
+    )
+    fun submitProfile(request: OnboardingRequest): ApiResponse<String>
 }
