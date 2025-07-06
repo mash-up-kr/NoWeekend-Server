06b45322c4db9d2f5c9eebb99eb4def886df7d0d
jay.park <once29@naver.com>
2025-07-07 00:07:41 +0900

    fix: request url encoded api format

--- noweekend-clients/client-oauth/src/main/kotlin/noweekend/client/google/GoogleTokenApi.kt
+++ noweekend-clients/client-oauth/src/main/kotlin/noweekend/client/google/GoogleTokenApi.kt
@@ -1,9 +1,10 @@
 package noweekend.client.google
 
+import feign.Headers
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RequestBody
 
 @FeignClient(
     value = "google-token-api",
@@ -15,11 +16,6 @@ internal interface GoogleTokenApi {
         consumes = [MediaType.APPLICATION_FORM_URLENCODED_VALUE],
         produces = [MediaType.APPLICATION_JSON_VALUE],
     )
-    fun getGoogleToken(
-        @RequestParam("code") code: String,
-        @RequestParam("redirect_uri") redirectUri: String,
-        @RequestParam("client_id") clientId: String,
-        @RequestParam("client_secret") clientSecret: String,
-        @RequestParam("grant_type") grantType: String = "authorization_code",
-    ): GoogleTokens
+    @Headers("Content-Type: application/x-www-form-urlencoded")
+    fun getGoogleToken(@RequestBody request: String): GoogleTokens
 }
