bdcaf34245392078669ee0916c602d642a4bde28
toychip <dlawnsgud427@naver.com>
2025-07-08 23:13:13 +0900

    test5

--- noweekend-clients/client-mcp/build.gradle.kts
+++ noweekend-clients/client-mcp/build.gradle.kts
@@ -4,6 +4,5 @@ dependencies {
     implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
     implementation("org.bouncycastle:bcpkix-jdk18on:1.78.1")
 
-    implementation("org.springframework.boot:spring-boot-starter-xml")
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-xml")
 }
