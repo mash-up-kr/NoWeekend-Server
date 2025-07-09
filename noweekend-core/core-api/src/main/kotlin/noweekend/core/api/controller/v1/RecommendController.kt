c4802027abbbcd3cda6ae9fd297e557695f22b57
toychip <dlawnsgud427@naver.com>
2025-07-09 14:59:20 +0900

    feat: recommend todo tag api

--- noweekend-core/core-api/src/main/kotlin/noweekend/core/api/controller/v1/RecommendController.kt
+++ noweekend-core/core-api/src/main/kotlin/noweekend/core/api/controller/v1/RecommendController.kt
@@ -1,5 +1,6 @@
 package noweekend.core.api.controller.v1
 
+import noweekend.client.mcp.recommend.model.TagApiResponses
 import noweekend.core.api.controller.v1.docs.RecommendControllerDocs
 import noweekend.core.api.controller.v1.response.WeatherApiResponse
 import noweekend.core.api.security.annotations.CurrentUserId
@@ -20,5 +21,14 @@ class RecommendController(
         @CurrentUserId userId: String,
     ): ApiResponse<WeatherApiResponse> {
         return ApiResponse.success(recommendService.getWeatherRecommend(userId))
+    }
+
+    @GetMapping("/todo")
+    override fun getTagRecommend(
+        @CurrentUserId userId: String,
+    ): ApiResponse<TagApiResponses> {
+        return ApiResponse.success(
+            recommendService.getTagRecommend(userId),
+        )
     }
 }
